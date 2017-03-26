7066f23b1ab8c73b1a21ae1995809aa31b5c74c8
Pawel <funky_pawel@o2.pl>
2017-03-26 20:21:00 +0200

    change to default content

--- src/test/java/TrainAwayTestNG/TestScript01.java
+++ src/test/java/TrainAwayTestNG/TestScript01.java
@@ -1,51 +1,85 @@
 package TrainAwayTestNG;
 
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxBinary;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
-import static TrainAwayTestNG.Utill.profilepath;
-
 /**
  * Created by Krzysztof on 2017-03-26.
  */
 public class TestScript01 {
-    private WebDriver driver; // Selenium control driver
-    private String baseUrl; // baseUrl of Trainaway website
+    public WebDriver driver;
+    public static String baseUrl;
 //test
 
+/**
+    @DataProvider(name = "Test")
+    public Object[][] testData(){
+
+        Object[][] data = new Object[4][2];
+
+        //1st row
+        data[0][0] = Utill.USER_NAME;
+        data[0][1] = Utill.PASSWORD;
+
+        //1st row
+        data[1][0] = "invalid";
+        data[1][1] = Utill.PASSWORD;
+
+        //1st row
+        data[2][0] = Utill.USER_NAME;
+        data[2][1] = "invalid";
+
+        //1st row
+        data[3][0] = "invalid";
+        data[3][1] = "invalid";
+
+    }
+    **/
     @BeforeMethod
     public void setUp() throws Exception {
 
-    String firefox = Utill.firefox;
-    String geckodriver = Utill.geckodriver;
-    System.setProperty(geckodriver,firefox);
+        String firefox = Utill.firefox;
+        String geckodriver = Utill.geckodriver;
+        System.setProperty(geckodriver,firefox);
 
-
-        FirefoxProfile firefoxProfile = new FirefoxProfile(new File(profilepath));
-        driver = new FirefoxDriver(firefoxProfile);
+        driver = new FirefoxDriver();
 
         baseUrl = Utill.BASE_URL;
         driver.manage().timeouts().implicitlyWait(Utill.WaitTime, TimeUnit.SECONDS);
         driver.get(baseUrl);
 
+        Alert alertOK = driver.switchTo().alert();
+        alertOK.accept();
+
+        driver.manage().window().maximize();
+
     }
-//jest to po to ze jak np. masz jakies inne przegladarki albo inne karty to sie selenium nie zgubi
     WebDriver getDriver(){
         return driver;
     }
 
-    @Test
-    public void testCase01(){
+   /** @AfterMethod
+    public void tearDown() throws Exception {
+        driver.quit();
+    }
+**/
+    @Test//(dataProvider = "Test")
+    public void testCase01() throws Exception {
 
-        getDriver().findElement(By.name("SIGN IN")).click();
+        driver.switchTo().defaultContent();
 
+        driver.findElement(By.xpath("/html/body/header/div[1]/div/div/div[3]/div[2]/div/a[2]")).click();
 
 
     }
--- src/test/java/TrainAwayTestNG/Utill.java
+++ src/test/java/TrainAwayTestNG/Utill.java
@@ -6,15 +6,20 @@ package TrainAwayTestNG;
 
 public class Utill {
 
-    //Valid web page URL
-    public static final String BASE_URL = "http://trainaway.kitmedia.pl/#/";
+    //Valid web page URL and pop up login and pass
+    public static final String BASE_URL = "http://rerere:rerere@trainaway.kitmedia.pl";
 
     // Valid pop up login and password
     public static final String POPUP_USER = "rerere";
     public static final String POPUP_PASS = "rerere";
 
+    // Valid login and password to website
+    public static final String USER_NAME = "XXX";
+    public static final String PASSWORD = "XXX";
+
 // firefox path
-    public static final String firefox = "C:\\Users\\Krzysztof\\Desktop\\geckodriver.exe";
+//    public static final String firefox = "C:\\Users\\Krzysztof\\Desktop\\gec15\\geckodriver.exe";
+    public static final String firefox = "C:\\Users\\Pawl\\Desktop\\driver\\geckodriver.exe"; //pawel
     public static final String geckodriver = "webdriver.gecko.driver";
 
     // Time to wait when searching
